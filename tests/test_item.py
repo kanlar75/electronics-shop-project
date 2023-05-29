2da2ad4020f9406bfb492d29a12764490568aeb1
kanlar75 <kanlar75@gmail.com>
2023-05-29 11:16:04 +0300

    написана часть тестов

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,23 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def creat_obj():
+    obj = Item("Смартфон", 10000, 20)
+    return obj
+
+
+def test_calculate_total_price(creat_obj):
+    assert creat_obj.calculate_total_price() == 200000
+
+
+def test_apply_discount(creat_obj):
+    Item.pay_rate = 0.5
+    assert creat_obj.apply_discount() == creat_obj.price
+
+
+def test_all(creat_obj):
+    assert len(Item.all) == 2
+    assert isinstance(Item.all, list)
