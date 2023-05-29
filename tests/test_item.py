376fcc48fcd23630561d13c343d2585c5b863b39
kanlar75 <kanlar75@gmail.com>
2023-05-29 12:55:30 +0300

    добавлены тесты по test_apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,3 +1,5 @@
+import pytest as pytest
+
 from src.item import Item
 
 
@@ -5,10 +7,11 @@ def test_calculate_total_price(creat_obj):
     assert creat_obj.calculate_total_price() == 200000
 
 
-def test_apply_discount(creat_obj):
-    Item.pay_rate = 0.7
+@pytest.mark.parametrize("pay_rate, price", [(0.7, 7000), (0.8, 8000)])
+def test_apply_discount(creat_obj, pay_rate, price):
+    Item.pay_rate = pay_rate
     creat_obj.apply_discount()
-    assert creat_obj.price == 7000
+    assert creat_obj.price == price
 
 
 def test_all(creat_obj):
