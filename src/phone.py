a9654572fcd44182cd6456f177f94bb06f69bed1
kanlar75 <kanlar75@gmail.com>
2023-06-12 08:06:48 +0300

    добавлены докстринги

--- src/phone.py
+++ src/phone.py
@@ -4,7 +4,7 @@ from src.item import Item
 class Phone(Item):
 
     def __init__(self, name, price, quantity, number_of_sim):
-        """ Инициализатор. """
+        """ Инициализатор Phone. """
 
         super().__init__(name, price, quantity)
 
