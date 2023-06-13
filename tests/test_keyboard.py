61cab0cef4e7c06e678e5f81dedee65ee88255f1
kanlar75 <kanlar75@gmail.com>
2023-06-13 06:39:29 +0300

    тесты класса KeyBoard

---
+++ tests/test_keyboard.py
@@ -0,0 +1,10 @@
+def test_init(test_obj_keyboard):
+    assert test_obj_keyboard.name == "Dark Project KD87A"
+    assert test_obj_keyboard._language == "EN"
+
+
+def test_change_lang(test_obj_keyboard):
+    test_obj_keyboard.change_lang()
+    assert test_obj_keyboard._language == "RU"
+    test_obj_keyboard.change_lang()
+    assert test_obj_keyboard._language == "EN"
