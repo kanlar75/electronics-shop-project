0a0ab3876f389d920fc6199acb71e024392670b8
kanlar75 <kanlar75@gmail.com>
2023-06-13 09:39:01 +0300

    *

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,20 +1,26 @@
 from src.item import Item
 
 
-class KeyBoard(Item):
-
-    def __init__(self, name: str, price: float, quantity: int, language="EN"):
-        super().__init__(name, price, quantity)
-        self._language = language
+class MixinLayout:
+    layout = None
 
     def change_lang(self):
         """ Метод для изменения языка (раскладки клавиатуры). """
 
-        if self.language == "EN":
+        if self._language == "EN":
             self._language = "RU"
         else:
             self._language = "EN"
+        MixinLayout.layout = self._language
         return self
+
+
+class KeyBoard(Item, MixinLayout):
+
+    def __init__(self, name: str, price: float, quantity: int, language="EN"):
+        super().__init__(name, price, quantity)
+        self._language = language
+        MixinLayout.layout = self._language
 
     @property
     def language(self):
