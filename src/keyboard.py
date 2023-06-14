08dbef036898872ec164bf6bb10a11d142c39c55
kanlar75 <kanlar75@gmail.com>
2023-06-14 09:36:17 +0300

    *

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,28 +2,26 @@ from src.item import Item
 
 
 class MixinLayout:
-    layout = None
+    layout = "EN"
 
     def change_lang(self):
         """ Метод для изменения языка (раскладки клавиатуры). """
 
-        if self._language == "EN":
+        if self.layout == "EN":
             self._language = "RU"
         else:
             self._language = "EN"
         MixinLayout.layout = self._language
-        return self
 
 
 class KeyBoard(Item, MixinLayout):
 
-    def __init__(self, name: str, price: float, quantity: int, language="EN"):
+    def __init__(self, name: str, price: float, quantity: int):
         super().__init__(name, price, quantity)
-        self._language = language
-        MixinLayout.layout = self._language
+        self._language = MixinLayout.layout
 
     @property
     def language(self):
-        """ Возвращает атрибут name. """
+        """ Возвращает атрибут language. """
 
         return f'{self._language}'
--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -9,3 +9,7 @@ def test_change_lang(test_obj_keyboard):
     assert test_obj_keyboard._language == "RU"
     test_obj_keyboard.change_lang()
     assert test_obj_keyboard._language == "EN"
+
+
+def test_language(test_obj_keyboard):
+    assert test_obj_keyboard.language == "EN"
